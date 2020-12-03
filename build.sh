c9f4827549828115f9ac9f94923ce1f7faa50479
pokoten <android.pokoten@gmail.com>
2020-12-03 13:02:15 +0900

    build.sh: added configure and save defconfig function.

--- build.sh
+++ build.sh
@@ -23,6 +23,8 @@ ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel3
 KERNEL_IMG=$KERNEL_DIR/output/arch/arm64/boot/Image.gz-dtb
 UPLOAD_DIR=$BASE_DIR/$DEVICE
 
+WLAN_MOD=$KERNEL_DIR/output/drivers/staging/qcacld-3.0/wlan.ko
+
 # Export
 export PATH="$BASE_DIR/toolchain/aarch64/aarch64-linux-android-4.9/bin:$PATH"
 export ARCH=arm64
@@ -31,13 +33,17 @@ export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 ## Functions ##
 
-# Make kernel
-function make_kernel() {
+# Initializing defconfig
+function init_defconfig(){
   echo -e "$cyan***********************************************"
   echo -e "          Initializing defconfig          "
   echo -e "***********************************************$nocol"
   #make $defconfig CC=clang O=output/
   make $defconfig O=output/
+}
+
+# Make kernel
+function make_kernel() {
   echo -e "$cyan***********************************************"
   echo -e "             Building kernel          "
   echo -e "***********************************************$nocol"
@@ -54,20 +60,14 @@ function make_kernel() {
 function config_kernel() {
   #make menuconfig CC=clang O=output/
   make menuconfig O=output/
-  echo -e "$cyan***********************************************"
-  echo -e "             Building kernel          "
-  echo -e "***********************************************$nocol"
-  #make -j$(nproc --all) CC=clang O=output/
-  make -j$(nproc --all) O=output/
-  if ! [ -a $KERNEL_IMG ];
-  then
-    echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
-    exit 1
-  fi
 }
 
 # Making zip
 function make_zip() {
+cp $WLAN_MOD $ANYKERNEL_DIR/modules/vendor/lib/modules/
+aarch64-linux-androidkernel-strip --strip-debug $ANYKERNEL_DIR/modules/vendor/lib/modules/wlan.ko
+$KERNEL_DIR/output/scripts/sign-file sha256 $KERNEL_DIR/output/certs/signing_key.pem $KERNEL_DIR/output/certs/signing_key.x509 $ANYKERNEL_DIR/modules/vendor/lib/modules/wlan.ko
+
 cp $KERNEL_IMG $ANYKERNEL_DIR
 mkdir -p $UPLOAD_DIR
 cd $ANYKERNEL_DIR
@@ -84,7 +84,8 @@ echo -e "$cyan***********************************************"
   echo -e " Select one of the following types of build : "
   echo -e " 1.Dirty"
   echo -e " 2.Clean"
-  echo -e " 3.Menuconfig"
+  echo -e " 3.Menuconfig & build"
+  echo -e " 4.Menuconfig & save defconfig (do not build)"
   echo -n " Your choice : ? "
   read ch
 
@@ -98,6 +99,7 @@ case $ch in
   1) echo -e "$cyan***********************************************"
      echo -e "          	Dirty          "
      echo -e "***********************************************$nocol"
+     init_defconfig
      make_kernel ;;
   2) echo -e "$cyan***********************************************"
      echo -e "          	Clean          "
@@ -106,11 +108,20 @@ case $ch in
      #make mrproper CC=clang O=output/
      make clean O=output/
      make mrproper O=output/
+     init_defconfig
      make_kernel ;;
   3) echo -e "$cyan***********************************************"
-     echo -e "          	Menuconfig     "
+     echo -e "          Menuconfig & build     "
      echo -e "***********************************************$nocol"
-     config_kernel ;;
+     config_kernel 
+     make_kernel ;;
+  4) echo -e "$cyan***********************************************"
+     echo -e "          Menuconfig & save defconfig     "
+     echo -e "***********************************************$nocol"
+     config_kernel 
+     make savedefconfig O=output/
+     echo -e "save defconfig and exit script."
+     exit 1;;
 esac
 
 if [ "$ziporkernel" = "1" ]; then
@@ -128,6 +139,7 @@ fi
 # Clean Up
 function cleanup(){
 rm -rf $ANYKERNEL_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL_DIR/modules/vendor/lib/modules/wlan.ko
 }
 
 options
